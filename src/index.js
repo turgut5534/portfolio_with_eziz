10d7947a114ad584ae4537c0b8e4c603239ae082
turgut5534 <turgutsalgin5534@gmail.com>
2023-04-19 02:48:59 +0300

    fix

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 3000
+
+const viewsDirectory = path.join(__dirname, '../templates/views')
+const publicDirectory = path.join(__dirname, '../public')
+
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', viewsDirectory)
+app.use(express.static(publicDirectory))
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
+app.listen(port , () => {
+    console.log(`Server is up on ${port}`)
+})
