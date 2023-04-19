1eb54dd251e2c6b33df755ca788c7ebd63179cfe
turgut5534 <turgutsalgin5534@gmail.com>
2023-04-19 03:13:32 +0300

    mysql connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 3000
+const db = require('./db/mysql')
+const User = require('./models/user')
 
 const viewsDirectory = path.join(__dirname, '../templates/views')
 const publicDirectory = path.join(__dirname, '../public')
@@ -11,7 +13,17 @@ app.set('views', viewsDirectory)
 app.use(express.static(publicDirectory))
 
 app.get('/', (req,res) => {
-    res.render('index')
+    User.create({
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'johndoe@example.com',
+  password: 'password123'
+}).then(user => {
+  console.log('User created:', user.toJSON());
+}).catch(error => {
+  console.error('Error creating user:', error);
+});
+
 })
 
 app.listen(port , () => {
