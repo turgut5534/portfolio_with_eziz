9ece65ccf28feb55dec59d2d29c04689c7377f5e
turgut5534 <turgutsalgin5534@gmail.com>
2023-04-21 22:27:14 +0300

    fix

--- public/admin/js/ajax.js
+++ public/admin/js/ajax.js
@@ -23,7 +23,7 @@ $('body').on('submit','#skill-save-form', function(e) {
                     <div class="d-flex justify-content-end">
                         <a href="javascript:;"
                             class="btn btn-sm btn-outline-secondary me-2 edit-skill edit-skill-${response.skill.id}" data-id="${response.skill.id}" data-title="${response.skill.title}" data-rate="${response.skill.rate}" data- data-bs-toggle="modal" data-bs-target="#editSkillModal">Edit</a>
-                        <a href="javascript:;" data-href="/admin/skills/delete/${response.skill.id}" data-name="${response.skill.title}" data-id="${response.skill.id}" class="btn btn-sm btn-outline-danger delete-skill">Delete</a>
+                        <a href="javascript:;" data-data="skill" data-href="/admin/skills/delete/${response.skill.id}" data-title="${response.skill.title}" data-id="${response.skill.id}" class="btn btn-sm btn-outline-danger delete-data">Delete</a>
                     </div>
                 </div>
             </div>
@@ -44,46 +44,6 @@ $('body').on('submit','#skill-save-form', function(e) {
     })
     
 })
-
-$('body').on('click', '.delete-skill', function(e) {
-
-    e.preventDefault()
-
-    const title = $(this).data('name')
-
-    Swal.fire({
-        title: 'Are you sure?',
-        text: title + " will be deleted",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-
-            const target = $(this).data('href')
-            const id = $(this).data('id')
-
-            $.ajax({
-                type: 'DELETE',
-                url: target,
-                success: function(response) {
-                    $('.skill-'+ id).remove()
-                },
-                error: function(e) {
-                    iziToast.error({
-                        title: 'Error',
-                        message: 'An error occured deleting the data',
-                    });
-                }
-            })
-
-        }
-      })
-    
-})
-
 
 $('body').on('click', '.edit-skill', function(e) {
 
@@ -137,13 +97,17 @@ $('body').on('submit','#skill-edit-form', function(e) {
     
 })
 
-$('body').on('click', '.delete-education', function(e) {
+
+$('body').on('click', '.delete-data', function(e) {
 
     e.preventDefault()
 
+    const data = $(this).data('data')
+    const title = $(this).data('title')
+
     Swal.fire({
         title: 'Are you sure?',
-        text: "Education will be deleted",
+        text: title+ " will be deleted",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -160,11 +124,11 @@ $('body').on('click', '.delete-education', function(e) {
                 url: target,
                 success: function(response) {
 
-                    $('.education-'+ id).remove()
+                    $('.'+data + '-' + id).remove()
 
                     iziToast.success({
                         title: 'Ok',
-                        message: 'Education is deleted successfully!',
+                        message: title + ' has been deleted successfully!',
                     });
                 },
                 error: function(e) {
